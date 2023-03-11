4bb47961acaa613556aad19b5db46fbf121ed382
Saddam Howladar <93901354+saddam757@users.noreply.github.com>
2023-03-11 18:52:37 +0600

    implement navigation drawer and logout functionality and also fix sign out dialog state at configuration change

--- app/src/main/java/com/shcoding/notediary/presentation/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/shcoding/notediary/presentation/screens/home/HomeViewModel.kt
@@ -1,6 +1,16 @@
 package com.shcoding.notediary.presentation.screens.home
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel: ViewModel() {
+
+    var signOutDialogState by mutableStateOf(false)
+    private set
+
+    fun isSignOutDialogOpened(signOutState: Boolean){
+        signOutDialogState = signOutState
+    }
 }
