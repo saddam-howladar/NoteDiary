2f98f315fdb2612337f9afe9e820507dfee712e0
Saddam Howladar <93901354+saddam757@users.noreply.github.com>
2023-03-12 18:26:46 +0600

    complete home screen design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,8 @@ android {
     kotlinOptions {
         allWarningsAsErrors = false
         freeCompilerArgs = freeCompilerArgs + listOf(
-            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
+            "-opt-in=androidx.compose.foundation.ExperimentalFoundationApi"
         )
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
--- app/src/main/java/com/shcoding/notediary/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/shcoding/notediary/presentation/screens/home/HomeScreen.kt
@@ -1,25 +1,31 @@
 package com.shcoding.notediary.presentation.screens.home
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Logout
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.shcoding.notediary.BuildConfig.APP_ID
+import com.shcoding.notediary.domain.model.Diary
+import com.shcoding.notediary.presentation.screens.home.components.DateHeader
 import com.shcoding.notediary.presentation.screens.home.components.HomeNavigationDrawer
 import com.shcoding.notediary.presentation.screens.home.components.HomeTopAppBar
 import com.shcoding.notediary.presentation.screens.home.components.SignOutAlertDialog
+import com.shcoding.notediary.presentation.screens.home.components.diary.DiaryHolder
 import io.realm.kotlin.mongodb.App
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.koin.androidx.compose.BuildConfig
 import org.koin.androidx.compose.koinViewModel
+import java.time.LocalDate
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -50,6 +56,8 @@ fun HomeScreen(
                 }
             }) {
 
+            DiaryPage()
+
 
         }
 
@@ -64,7 +72,7 @@ fun HomeScreen(
 
                     user?.let {
                         it.logOut()
-                        withContext(Dispatchers.Main){
+                        withContext(Dispatchers.Main) {
                             navigateToAuth()
                         }
 
@@ -75,4 +83,61 @@ fun HomeScreen(
 
             })
     }
+}
+
+
+@Composable
+fun DiaryPage(
+    diaryNotes: Map<LocalDate, List<Diary>> = mapOf(),
+    onClick: () -> Unit = {}
+) {
+
+    if (diaryNotes.isNotEmpty()) {
+
+        LazyColumn(modifier = Modifier.padding(horizontal = 24.dp)) {
+            diaryNotes.forEach { (localDate, diaries) ->
+
+                stickyHeader(key = localDate) {
+                    DateHeader(localDate = localDate)
+                }
+                items(items = diaries, key = { it._id }) {
+                    DiaryHolder(diary = it, onClick = { onClick() })
+                }
+
+            }
+        }
+
+    } else {
+        EmptyPage()
+    }
+
+}
+
+@Composable
+fun EmptyPage(
+    title: String = "Empty Diary",
+    subTitle: String = "Write something"
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = title,
+            fontSize = MaterialTheme.typography.titleMedium.fontSize,
+            fontWeight = FontWeight.Medium
+        )
+        Text(
+            text = subTitle,
+            fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+            fontWeight = FontWeight.Normal
+        )
+
+    }
+
 }
